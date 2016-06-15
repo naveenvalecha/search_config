44fe86940251bc1567e017d9c152e95a05cc74f5
joyceg <geojoyce934@gmail.com>
2016-06-15 18:58:46 +0530

    fixed some database query ports
    Needs review for the search configuration Port (#4)
    
    * Search Configuration Port
    
    added DB query corrections, namespace inclusion, hook permissions

---
+++ src/searchConfigPermissions.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace  Drupal\search_config;
+class searchConfigPermissions {
+    public function permissions() {
+        $permissions = [];
+        foreach (search_config_content_types() as $type => $label) {
+            $permissions["search $type content"] = array(
+                'title' => t('%type_name: Search content of this type', array('%type_name' => $label)),
+            );
+        }
+        return $permissions;
+    }
+    public function search_config_content_types()  {
+        return array_map('check_plain', node_type_get_names());
+    }
+}
