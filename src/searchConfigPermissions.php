421144aa24214c243ad0a4e39acd6ac96a5b8cf6
joyceg <geojoyce934@gmail.com>
2016-06-24 00:16:31 +0530

    added a PHP unit test for default settings and a namespace for .module

--- src/searchConfigPermissions.php
+++ src/searchConfigPermissions.php
@@ -10,7 +10,7 @@ namespace  Drupal\search_config;
 class searchConfigPermissions {
     public function permissions() {
         $permissions = [];
-        foreach (search_config_content_types() as $type => $label) {
+        foreach (\Drupal\module\search_config_content_types() as $type => $label) {
             $permissions["search $type content"] = array(
                 'title' => t('%type_name: Search content of this type', array('%type_name' => $label)),
             );
