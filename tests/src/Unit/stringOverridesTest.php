48692adb5d34462ac9ac35160b29e7fe0de4c1c1
Joyce George <geojoyce934@gmail.com>
2016-07-08 14:02:04 +0530

    Testing String overrides (#16)
    * Testing String overrides

---
+++ tests/src/Unit/stringOverridesTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Testing the string overrides: search_config_string_overrides()
+ */
+use \Drupal\Tests;
+class stringOverridesTest extends \PHPUnit_Framework_TestCase {
+    public $overrides = [];
+    public function testStringOverrides() {
+        $this->overrides = search_config_string_overrides();
+        $arr= [[],[],[],[],[],[],[],[],[],[],[],'default','default','default','default','default','default',[],[],];
+        $this->assertTrue(arraysAreSimilar($arr, $this->overrides));
+        function arraysAreSimilar($a, $b) {
+            // if the indexes don't match, return immediately
+            if (count(array_diff_assoc($a, $b))) {
+                return false;
+            }
+            // compare the values between the two arrays, the indexes match.
+            foreach($a as $k => $v) {
+                if ($v !== $b[$k]) {
+                    return false;
+                }
+            }
+            // we have identical indexes, and no unequal values
+            return true;
+        }
+
+    }
+}
+?>
